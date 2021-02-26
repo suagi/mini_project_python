a95856fa5d1b776c14178525e8b24934e477901e
suagi <df.suagi@gmail.com>
2021-02-26 17:19:04 +0900

    mini project

---
+++ mini_project/숫자 맞히기 게임.py
@@ -0,0 +1,21 @@
+import random
+
+# 코드를 작성하세요.
+chance = 4
+result = random.randint(1, 20)
+print(result)
+
+for i in range(chance):
+    num = int(input("기회가 {}번 남았습니다. 1-20 사이의 숫자를 맞혀 보세요: ".format(chance)))
+    if result > num:
+        chance -= 1
+        print("Up")
+        continue
+    elif result < num:
+        chance -= 1
+        print("Down")
+        continue
+    elif result == num:
+        print("축하합니다. {}번만에 숫자를 맞히셨습니다.".format(i + 1))
+        break
+    print("아쉽습니다. 정답은 {}입니다.".format(result))
