aecf12128abfbd37157bab1e88a958167421d9b8
suagi <df.suagi@gmail.com>
2021-02-26 19:56:27 +0900

    mini project

---
+++ 단어장 만들기/main.py
@@ -0,0 +1,11 @@
+
+with open('vocabulary.txt', 'w', encoding='UTF-8') as eng_kor:
+    while True:
+        word = input('영어 단어를 입력하세요: ')
+        if word =='q':
+            break
+        answer = input('뜻을 입력하세요: ')
+        if answer == 'q':
+            break
+
+        eng_kor.write("{} : {}\n".format(word, answer))
