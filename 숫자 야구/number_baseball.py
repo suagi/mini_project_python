c4a931a0197c748a4deac148563c739f9f0c3d65
suagi <df.suagi@gmail.com>
2021-03-02 23:32:01 +0900

    number baseball

--- 숫자 야구/number_baseball.py
+++ 숫자 야구/number_baseball.py
@@ -10,4 +10,18 @@ def generate_numbers():
     print("0과 9 사이의 서로 다른 숫자 3개를 랜덤한 순서로 뽑았습니다.\n")
     return numbers
 
-print(generate_numbers())
+
+def take_guess():
+    print("숫자 3개를 하나씩 차례대로 입력하세요.")
+    new_guess = []
+
+    while len(new_guess) < 3:
+        A = int(input(f"{len(new_guess) + 1}번째 숫자를 입력하세요: "))
+        if A not in new_guess and 0 <= A < 10:
+            new_guess.append(A)
+        elif not 0 <= A < 10:
+            print("범위를 벗어나는 숫자입니다. 다시 입력하세요.")
+        elif A in new_guess:
+            print("중복되는 숫자입니다. 다시 입력하세요.")
+
+    return new_guess
