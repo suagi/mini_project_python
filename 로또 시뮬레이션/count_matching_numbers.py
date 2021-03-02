cad0205ac58c27e6b3d874b59269fc462c26a314
suagi <df.suagi@gmail.com>
2021-03-02 22:16:32 +0900

    Lotto Simulation

--- 로또 시뮬레이션/count_matching_numbers.py
+++ 로또 시뮬레이션/count_matching_numbers.py
@@ -4,6 +4,11 @@ from draw_winning_numbers import draw_winning_numbers as draw
 def count_matching_numbers():
     my_num = gen(6)
     com_num = draw()
+    count = 0
 
-    print(my_num)
-    print(com_num)
+    for i in my_num:
+        if i in com_num:
+            count += 1
+    return count
+
+print(count_matching_numbers())
---
+++ 로또 시뮬레이션/main.py
@@ -0,0 +1,46 @@
+import random
+
+def generate_numbers(n):
+    generate_list = []
+    while len(generate_list) < n:
+        select = random.randint(1, 45)
+        if select not in generate_list:
+            generate_list.append(select)
+
+    return generate_list
+
+def draw_winning_numbers():
+    winning_numbers = generate_numbers(7)
+    return sorted(winning_numbers[:6]) + winning_numbers[6:]
+
+def count_matching_numbers(my, com):
+    count = 0
+
+    for i in my:
+        if i in com:
+            count += 1
+
+    return count
+
+def check(my, com):
+    result = count_matching_numbers(my, com[:6])
+    bonus = count_matching_numbers(my, com[6:])
+    print(my)
+    print(com[:6])
+    print(com[6:])
+    print(result)
+
+    if result == 6:
+        return 1000000000
+    elif result == 5 and bonus == 1:
+        return 50000000
+    elif result == 5:
+        return 1000000
+    elif result == 4:
+        return 50000
+    elif result == 3:
+        return 5000
+    else:
+        return '꽝'
+
+print(check(generate_numbers(6), draw_winning_numbers()))
